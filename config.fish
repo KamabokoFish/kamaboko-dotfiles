8f2f543fbe971c5f0c1194bc68ecfb44a110bff1
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2023-04-06 16:13:02 +0900

    refactor(config): some refactor gitconfig and config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias python='python3'
+    alias tp='trash-put'
+    alias dstart='systemctl --user start docker-desktop'
+    alias dstop='systemctl --user stop docker-desktop'
     alias la='exa -la --icons'
     alias ll='exa -l --icons'
     alias lt='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
