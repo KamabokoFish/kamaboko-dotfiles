091f25ae4b36690184091d6de5a662e03b7bf459
kamaboko <kadoma.fukakusa248@gmail.com>
2024-10-04 21:22:06 +0900

    refactor(readme): add some memo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### dotfiles
 
-My dotfiles repository!
-(｡>﹏<｡)
+`init.sh`
+- Homeディレクトリ直下に設定ファイルのリンクを貼る
+- 一部のファイルについては作成しないため注意
+
+`init-config.sh`
+- `.config/`以下に配置される設定ファイルを、個別のGithubリポジトリから取ってきて`$HOME/.config/`以下に配置する
--- init.sh
+++ init.sh
@@ -2,10 +2,13 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Homeディレクトリ直下にリンクを作成
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+    # VSCodeの設定は今の所Github経由の同期機能で処理してるため、スルー
+    [[ "$dotfile" == "${SCRIPT_DIR}/.settings.json" ]] && continue
 
     ln -fnsv "$dotfile" "$HOME"
 done
