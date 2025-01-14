29039cded75fa4324f265cbab88e46bf940f9333
kamaboko <79447012+KamabokoFish@users.noreply.github.com>
2025-01-14 21:16:53 +0900

    refactor(wezterm): change fontsize

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,10 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.initial_cols = 130
-config.initial_rows = 50
+config.initial_cols = 135
+config.initial_rows = 52
 config.color_scheme = 'Ayu Mirage'
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.9
 config.window_padding = {
   left = 0,
   right = 0,
@@ -15,11 +15,11 @@ config.window_padding = {
   bottom = 0,
 }
 
-config.macos_window_background_blur = 50
+config.macos_window_background_blur = 30
 -- フォントの設定
 config.font = wezterm.font("UDEV Gothic 35NF", { weight = "Bold" })
 -- フォントサイズの設定
-config.font_size = 16.5
+config.font_size = 17
 
 -- and finally, return the configuration to wezterm
 return config
