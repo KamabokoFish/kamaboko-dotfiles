26e02d81242a6fe1cd4dcf806948cb6d5601bbc6
kamaboko <79447012+KamabokoFish@users.noreply.github.com>
2025-04-10 17:21:21 +0900

    refactor(git): change default merge editor

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.initial_cols = 135
-config.initial_rows = 52
+config.initial_rows = 55
 config.color_scheme = 'Ayu Mirage'
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.window_padding = {
   left = 0,
   right = 0,
@@ -15,11 +15,29 @@ config.window_padding = {
   bottom = 0,
 }
 
-config.macos_window_background_blur = 30
+config.macos_window_background_blur = 50
 -- フォントの設定
 config.font = wezterm.font("UDEV Gothic 35NF", { weight = "Bold" })
 -- フォントサイズの設定
-config.font_size = 17
+config.font_size = 16
+
+-- config.leader = { key = 'g', mods = 'CTRL', timeout_milliseconds = 1500 }
+
+config.keys = {
+  {
+    key = '%',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '"',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
+
+
+
 
 -- and finally, return the configuration to wezterm
 return config
